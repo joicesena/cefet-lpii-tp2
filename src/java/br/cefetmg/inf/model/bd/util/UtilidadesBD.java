e88e966366ac779c8ea973dc80e628b233bc15ae
Nícolas (aka. Gzus) <nic.cp@hotmail.com>
2018-07-29 11:58:19 -0300

    Adiciona métodos úteis em 'UtilidadesBD'

--- src/java/br/cefetmg/inf/model/bd/util/UtilidadesBD.java
+++ src/java/br/cefetmg/inf/model/bd/util/UtilidadesBD.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -33,8 +34,36 @@ public final class UtilidadesBD {
         return senhaEmSHA256;
     }
 
-    public static boolean verificaSenha(String senha, String senhaConvertida) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public static boolean verificaSenha(String senha, String senhaConvertida)
+            throws NoSuchAlgorithmException, UnsupportedEncodingException {
         return senhaConvertida.equals(stringParaSHA256(senha));
+    }
+
+    public static int buscaUltimoRegistroRelacionadoAoQuarto(int nroQuarto)
+            throws SQLException {
+        String qry = "SELECT A.seqHospedagem "
+                + "FROM Hospedagem A "
+                + "JOIN QuartoHospedagem B ON A.seqHospedagem = B.seqHospedagem "
+                + "WHERE B.nroQuarto = ? "
+                + "ORDER BY A.datCheckIn DESC"
+                + "LIMIT 1";
+        PreparedStatement pStmt = con.prepareStatement(qry);
+        pStmt.setInt(1, nroQuarto);
+        ResultSet rs = pStmt.executeQuery();
+
+        return rs.getInt(1);
+    }
+    
+    public static ResultSet retornaRelatorioDespesas(int seqHospedagem, int nroQuarto) throws SQLException {
+        String qry = "SELECT * "
+                + "FROM  relatorioDespesas "
+                + "WHERE seqHospedagem = ? AND nroQuarto = ?";
+        PreparedStatement pStmt = con.prepareStatement(qry);
+        pStmt.setInt(1, seqHospedagem);
+        pStmt.setInt(2, nroQuarto);
+        ResultSet rs = pStmt.executeQuery();
+
+        return rs;
     }
 
     public static void apagarBDHosten() throws SQLException {
