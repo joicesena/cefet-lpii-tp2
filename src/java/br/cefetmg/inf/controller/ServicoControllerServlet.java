bf0e280b979d549b133233760192e185c539e9b1
Lívia Delgado <livia.delgado.ac@gmail.com>
2018-07-31 07:23:21 -0300

    Atualiza Controllers - Termina serviços

--- src/java/br/cefetmg/inf/controller/ServicoControllerServlet.java
+++ src/java/br/cefetmg/inf/controller/ServicoControllerServlet.java
@@ -1,20 +1,32 @@
 package br.cefetmg.inf.controller;
 
+import br.cefetmg.inf.model.bd.dao.ServicoAreaDAO;
 import br.cefetmg.inf.model.bd.dao.ServicoDAO;
+import br.cefetmg.inf.model.bd.dao.UsuarioDAO;
+import br.cefetmg.inf.model.bd.util.UtilidadesBD;
 import br.cefetmg.inf.model.dto.Servico;
+import br.cefetmg.inf.model.dto.ServicoArea;
+import br.cefetmg.inf.model.dto.Usuario;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
-import javax.servlet.RequestDispatcher;
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(name = "ServicoControllerServlet", urlPatterns = {"/servico"})
 public class ServicoControllerServlet extends HttpServlet {
     private HttpServletRequest requestInterno;
-    private String operacaoServico;
+    private int operacaoRegistro;
+    
+    private String codRegistroSelecionado;
     
     private ServicoDAO servico;
     
@@ -23,57 +35,124 @@ public class ServicoControllerServlet extends HttpServlet {
         servico = ServicoDAO.getInstance();
     }
     
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         this.requestInterno = request;
-        operacaoServico = null;
+        operacaoRegistro = 0;
 
-        operacaoServico = requestInterno.getParameter("operacaoServico");
-        
+        operacaoRegistro = Integer.parseInt(requestInterno.getParameter("operacaoItem"));
+        JsonObject retorno;
         try {
-            if (operacaoServico.equals("cadastrarServico")) {
-                cadastraServico();
-            } else if (operacaoServico.equals("pesquisarServico")) {
-                pesquisaServico();
-            } else if (operacaoServico.equals("editarServico")) {
-                editaServico();
-            } else if (operacaoServico.equals("removerServico")) {
-                removeServico();
+            if (operacaoRegistro == 1) {
+                codRegistroSelecionado = request.getParameter("seqServico");
+                retorno = retornarDadosRegistro(codRegistroSelecionado);
+                response.setContentType("text/json");
+                PrintWriter out = response.getWriter();
+                out.print(retorno);
+            } else if (operacaoRegistro == 2) {
+                retorno = inserirRegistro();
+                response.setContentType("text/json");
+                PrintWriter out = response.getWriter();
+                out.print(retorno);
+            } else if (operacaoRegistro == 3) {
+                pesquisarRegistro();
+            } else if (operacaoRegistro == 4) {
+                retorno = editarRegistro();
+                response.setContentType("text/json");
+                PrintWriter out = response.getWriter();
+                out.print(retorno);
+            } else if (operacaoRegistro == 5) {
+                retorno = removerRegistro();
+                response.setContentType("text/json");
+                PrintWriter out = response.getWriter();
+                out.print(retorno);
             }
         } catch (SQLException exc) {
             //
             //
             //
+        } catch (NoSuchAlgorithmException ex) {
+            //
+            //
+            //
+        } catch (UnsupportedEncodingException ex) {
+            //
+            //
+            //
         }
-        
-        String caminhoArquivo = "/view/servicos.jsp";
-        
-        RequestDispatcher rd = request.getRequestDispatcher(caminhoArquivo);
-        rd.forward(request, response);
         
     }
 
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
     
     //
     // MÉTODOS DE CONTROLE
     //
-    private void cadastraServico () throws SQLException {
+    private JsonObject retornarDadosRegistro (String codRegistro) throws SQLException {
+        Servico [] registros = servico.busca("seqServico", Integer.parseInt(codRegistro));
+
+        Servico registroRetorno = registros[0];
+
+        ServicoAreaDAO areaDAO = ServicoAreaDAO.getInstance();
+        ServicoArea [] area = areaDAO.busca("codServicoArea", registroRetorno.getCodServicoArea());
+        String nomServicoArea = area[0].getNomServicoArea();
+
+        JsonObject dadosRegistro = Json.createObjectBuilder()
+            .add("seqServico", registroRetorno.getSeqServico())
+            .add("desServico", registroRetorno.getDesServico())
+            .add("vlrUnit", registroRetorno.getVlrUnit())
+            .add("nomServicoArea", nomServicoArea)
+            .build();
+
+        return dadosRegistro;
+    }
+
+    private JsonObject inserirRegistro () throws SQLException {
         String desServico;
         Double vlrUnit;
         String codServicoArea;
+        String nomServicoArea;
         
         desServico = requestInterno.getParameter("desServico");
         vlrUnit = Double.parseDouble(requestInterno.getParameter("vlrUnit"));
-        codServicoArea = requestInterno.getParameter("codServicoArea");
+        nomServicoArea = requestInterno.getParameter("nomServicoArea");
+        
+        ServicoAreaDAO areaDAO = ServicoAreaDAO.getInstance();
+        ServicoArea[] areas = areaDAO.busca("nomServicoArea", nomServicoArea);
+        codServicoArea = areas[0].getCodServicoArea();
+        
+        JsonObject dadosRegistro;
         
         Servico servicoAdicionar = new Servico(desServico, vlrUnit, codServicoArea);
-        servico.adiciona(servicoAdicionar);
+        boolean testeRegistro = servico.adiciona(servicoAdicionar);
         
-        return;
+        if (testeRegistro) {
+            dadosRegistro = Json.createObjectBuilder()
+                .add("success", true)
+                .add("mensagem", "Registro adicionado com sucesso!")
+                .build();
+        } else {
+            dadosRegistro = Json.createObjectBuilder()
+                .add("success", false)
+                .add("mensagem", "Ocorreu erro ao adicionar o registro. Repita a operação.")
+                .build();
+        }
+
+        return dadosRegistro;
     }
     
-    private void pesquisaServico() throws SQLException {
+    private void pesquisarRegistro() throws SQLException {
         String parametroPesquisa, tipoParametroPesquisa;
         parametroPesquisa = (String)requestInterno.getParameter("pesquisaServico");
         tipoParametroPesquisa = (String)requestInterno.getParameter("parametroPesquisaServico");
@@ -84,29 +163,75 @@ public class ServicoControllerServlet extends HttpServlet {
         return;
     }
 
-    private void editaServico() throws SQLException {
+    private JsonObject editarRegistro() throws SQLException {
         String desServico;
         Double vlrUnit;
         String codServicoArea;
 
-        desServico = requestInterno.getParameter("desServicoSelecionado");
-        vlrUnit = Double.parseDouble(requestInterno.getParameter("vlrUnitSelecionado"));
-        codServicoArea = requestInterno.getParameter("codServicoAreaSelecionado");
+        desServico = requestInterno.getParameter("desServico");
+        vlrUnit = Double.parseDouble(requestInterno.getParameter("vlrUnit"));
+        String nomServicoArea = requestInterno.getParameter("nomServicoArea");
         
-        Servico servicoAtualizado = new Servico(desServico, vlrUnit, codServicoArea);
+        ServicoAreaDAO areaDAO = ServicoAreaDAO.getInstance();
+        ServicoArea[] areas = areaDAO.busca("nomServicoArea", nomServicoArea);
+        codServicoArea = areas[0].getCodServicoArea();
+
+        Servico registroAtualizado = new Servico(desServico, vlrUnit, codServicoArea);
         
-////        servico.atualiza();
-        
-        return;
+        JsonObject dadosRegistro;
+
+        boolean testeRegistro = servico.atualiza(codRegistroSelecionado, registroAtualizado);
+        if (testeRegistro) {
+            dadosRegistro = Json.createObjectBuilder()
+                .add("sucesso", true)
+                .add("mensagem", "Registro alterado com sucesso!")
+                .build();
+        } else {
+            dadosRegistro = Json.createObjectBuilder()
+                .add("sucesso", false)
+                .add("mensagem", "Ocorreu erro ao alterar o registro. Repita a operação.")
+                .build();
+        }
+
+        return dadosRegistro;
     }
     
-    private void removeServico() throws SQLException {
-//        String codServico;
-//        codServico = requestInterno.getParameter("codServicoSelecionado");
-//        
-//        servico.deleta(codServico);
+    private JsonObject removerRegistro() throws SQLException, NoSuchAlgorithmException, UnsupportedEncodingException {
+        Servico [] registroBuscado = servico.busca("seqServico", codRegistroSelecionado);
+
+        HttpSession session = requestInterno.getSession();
         
-        return;
+        UsuarioDAO usuarioDAO = UsuarioDAO.getInstance();
+        Usuario[] usuarios = usuarioDAO.busca("codUsuario", session.getAttribute("codUsuario"));
+        System.out.println("usuarios.length "+usuarios.length);
+        
+        String senhaSHA256 = requestInterno.getParameter("senhaFuncionario");
+        String senha = UtilidadesBD.stringParaSHA256(senhaSHA256);
+        
+        JsonObject dadosRegistro;
+
+        if ((usuarios[0].getDesSenha()).equals(senha)) {
+            boolean testeExclusaoItem = servico.deleta(codRegistroSelecionado);
+            
+            if (testeExclusaoItem) {
+                dadosRegistro = Json.createObjectBuilder()
+                    .add("sucesso", true)
+                    .add("mensagem", "Registro excluído com sucesso!")
+                    .build();
+            } else {
+                dadosRegistro = Json.createObjectBuilder()
+                    .add("sucesso", false)
+                    .add("mensagem", "Ocorreu erro ao excluir o registro. Repita a operação.")
+                    .build();
+            }
+        } else {
+            dadosRegistro = Json.createObjectBuilder()
+                .add("sucesso", false)
+                .add("mensagem", "Senha inválida")
+                .build();
+        }
+
+        return dadosRegistro;
     }
 
 }
--- src/java/br/cefetmg/inf/controller/util/ListaRegistrosComboBox.java
+++ src/java/br/cefetmg/inf/controller/util/ListaRegistrosComboBox.java
@@ -1,24 +1,42 @@
 package br.cefetmg.inf.controller.util;
 
-import br.cefetmg.inf.model.bd.dao.ItemConfortoDAO;
-import br.cefetmg.inf.model.dto.ItemConforto;
+import br.cefetmg.inf.model.bd.dao.*;
+import br.cefetmg.inf.model.dto.*;
 import java.sql.SQLException;
 import javax.json.Json;
 import javax.json.JsonArray;
 
 public class ListaRegistrosComboBox {
     
-    public JsonArray listaItensComboBox () throws SQLException {
-        ItemConfortoDAO itemDAO = ItemConfortoDAO.getInstance();
-        ItemConforto[] itens = itemDAO.busca();
+    public JsonArray listarItensConfortoComboBox () throws SQLException {
+        ItemConfortoDAO registroDAO = ItemConfortoDAO.getInstance();
+        ItemConforto[] registros = registroDAO.busca();
         
         JsonArray arrayJson = Json.createArrayBuilder().build();
         
-        for (ItemConforto item : itens) {
+        for (ItemConforto registro : registros) {
             arrayJson.add(
                 (Json.createObjectBuilder()
-                    .add("codigoItem", item.getCodItem())
-                    .add("descricaoItem", item.getDesItem())
+                    .add("codigoItem", registro.getCodItem())
+                    .add("descricaoItem", registro.getDesItem())
+                ).build()
+            );
+        }
+        
+        return arrayJson;
+    }
+
+    public JsonArray listarProgramasComboBox () throws SQLException {
+        ProgramaDAO registroDAO = ProgramaDAO.getInstance();
+        Programa [] registros = registroDAO.busca();
+        
+        JsonArray arrayJson = Json.createArrayBuilder().build();
+        
+        for (Programa registro : registros) {
+            arrayJson.add(
+                (Json.createObjectBuilder()
+                    .add("codPrograma", registro.getCodPrograma())
+                    .add("desPrograma", registro.getDesPrograma())
                 ).build()
             );
         }
